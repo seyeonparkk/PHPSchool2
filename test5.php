22fd2e32b352612912cc52b0d0ddf7aeffd32856
parkseyeon06 <psyvitamin@gmail.com>
2023-04-21 13:54:25 +0900

    test

--- test5.php
+++ test5.php
@@ -22,22 +22,50 @@ $result = strcmp($var1, $var2);
 echo "result : ".$result;
 
 ?>
+<hr>
 <?php
-$str="abcdef";
-$find="c";
-$pos=$strpos($str,$find);
-echo "pos".$pos;
+$str = "abcdef";
+$find = "c";
+$pos = strpos($str,$find);
+echo "pos: ".$pos;
+?>
+
+<hr>
+<?php
+$str = "Mary had a Llittle lamb and she loved IT so MUCH<br/>";
+$low = strtolower($str);
+$up = strtoupper($str);
+echo "lower : ".$low."<br/>";
+echo "upper : ".$up."<br/>";
+?>
+
+<hr>
+<?php
+$str = "hellophpwelcome";
+$result = substr($str,5,5);
+echo $result."<br/>";
+
+$result2= substr($str, 3);
+echo $result2;
 ?>
 <hr>
 <?php
-$str="mary had a little lamb and she loved it so MUCH";
-echo strtolower($str)."<br/>";
-echo strtoupper($str);
+$arr=array("pizza1","pizza2","pizza3","pizza4","pizza5");
+$str = implode(",",$arr);
+echo "implode:".$str;
+?>
+
+<hr>
+<?php
+$str = "apple/mango/strawberry/melon";
+$str=explode("/",$str);
+for ( $s : $str) {
+    echo "explode:".$s;
+}
 ?>
 <?php
-$str="hellophpwelcome";
-$result=substr($str,5,5);
-echo $result;    //phpwe
-$result2=substr($str,3);
-echo $result2;
+$txt="You should eat fruits, shoudl not buy";
+$result=str_replace("should","cluld",$txt);
+echo "변경전 문자열 : ".$txt;
+echo "변경후 문자열 : ".$result;
 ?>
