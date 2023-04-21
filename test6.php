1aa51096bcce5ef82e1cdb6f7f8d4da4606f9548
parkseyeon06 <psyvitamin@gmail.com>
2023-04-21 14:29:27 +0900

    feop

---
+++ test6.php
@@ -0,0 +1,30 @@
+<?php
+//저장
+$Filename="data.txt";
+// $fp=fopen($Filename,"w");
+// $str="Mary had a dog";
+// $str1="tom had a cat";
+// fwrite($fp,$str1);
+// fclose($fp);
+// echo"저장되었습니다"
+
+
+//파일읽기
+$fp=fopen($Filename,"r");
+while(!feof($fp)){   //end of file 파일을 가리키는게 끝이 아니면
+    //echo fgetc($fp);
+    echo fgets($fp);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+?>
